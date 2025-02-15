5ba8d62d06ffe9ab2b1dae6638bb96c5fd012daa
watsonbl <watsonbl@OA-S145-01>
2025-02-15 15:31:44 -0500

    Added two buttons to swap between two birds and a button that will eventually make the bird sing

--- Assets/Scripts/SpawnNote.cs
+++ Assets/Scripts/SpawnNote.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SpawnNote : MonoBehaviour
 {
     public GameObject note;
+    Vector3 pos;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,12 +16,15 @@ public class SpawnNote : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             
-            GameObject newNote = Instantiate(note, transform.position, Quaternion.identity);
+            pos.x = transform.localPosition.x + Random.Range(-2, 2);
+            pos.y = transform.localPosition.y + Random.Range(-2, 2);
+            pos.z = transform.localPosition.z + 0;
+            GameObject newNote = Instantiate(note, pos, Quaternion.identity);
             
-            Destroy(newNote, 6);
+            Destroy(newNote, 3);
         }
     }
 }
